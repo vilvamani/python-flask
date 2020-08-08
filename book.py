c74dc6659b1e6d6cbd76575efc991640ac2ea1dd
armugamv <vilvamani007@gmail.com>
2020-08-08 12:06:32 +0530

    re-architech the code

---
+++ book.py
@@ -0,0 +1,78 @@
+from flask_restful import Resource, reqparse
+from flask import jsonify, make_response
+from flask_httpauth import HTTPBasicAuth
+
+# Create some test data for our catalog in the form of a list of dictionaries.
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'year_published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'year_published': '1975'}
+]
+
+parser = reqparse.RequestParser()
+parser.add_argument('title', help = 'This field cannot be blank', required = True)
+parser.add_argument('author', help = 'This field cannot be blank', required = True)
+parser.add_argument('first_sentence', help = 'This field cannot be blank', required = True)
+parser.add_argument('year_published', help = 'This field cannot be blank', required = True)
+
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'vilva':
+        return 'devops'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+
+
+class AllBooks(Resource):
+    @auth.login_required
+    def get(self):
+        return jsonify(books)
+
+class Book(Resource):
+    @auth.login_required
+    def get(self, book_id):
+        book = [book for book in books if book['id'] == book_id]
+        if len(book) == 0:
+            return "Error: No id field provided. Please specify an id."
+        return jsonify({'Book': book[0]})
+
+    @auth.login_required
+    def delete(self, book_id):
+        book = [book for book in books if book['id'] == book_id]
+        if len(book) == 0:
+            return "Error: Book not found"
+        books.remove(book[0])
+        return jsonify({'result': True})
+
+class NewBook(Resource):
+    @auth.login_required
+    def post(self):
+        data = parser.parse_args()
+
+        book = {
+            'id': books[-1]['id'] + 1,
+            'title': data['title'],
+            'author': data['author'],
+            'first_sentence': data['first_sentence'],
+            'published': data['year_published']
+        }
+
+        books.append(book)
+        return {'Book': book}, 201
---
+++ run.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, make_response, jsonify
+from flask_restful import Api
+import book
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+api = Api(app)
+api.add_resource(book.AllBooks, '/api/v1/books')
+api.add_resource(book.Book, '/api/v1/book/<int:book_id>')
+api.add_resource(book.NewBook, '/api/v1/book')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return make_response(jsonify({'error': 'The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.'}), 404)
+
+
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call  
+# the associated function.
+# ‘/’ URL is bound with home() function.
+@app.route('/', methods=['GET'])
+def home():
+    return make_response(jsonify({'Message': 'Python Flask App'}))
+
+# main driver function
+if __name__ == '__main__':
+    # run() method of Flask class runs the application
+    # on the local development server. 
+    app.run(host='0.0.0.0')
